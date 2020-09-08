93d6daac82e1600d9fc41c79a7de0fb556776ce3
Andrew Weiss <wandrew8@gmail.com>
2020-09-08 12:21:39 -0700

    adds a doughnut and radar chart

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const ctx = document.querySelector("#lineChart");
 const ctx2 = document.querySelector("#barChart");
-
+const ctx3 = document.querySelector("#radarChart");
+const ctx4 = document.querySelector("#donutChart");
 const title = document.querySelector(".title");
 
 let months = ["January", "February", "March", "April", "May", "June", "July"];
@@ -15,13 +16,13 @@ let lineChart = new Chart(ctx, {
         labels: months,
         datasets: [
             {
-                label: "First Line Chart",
+                label: "2019",
                 lineTension: 0,
                 backgroundColor: "rgba(50,45,100,0.5)",
                 data: [65, 59, 67, 45, 67, 55, 56]
             },
             {
-                label: "Second Line Chart",
+                label: "2020",
                 lineTension: 0,
                 backgroundColor: "rgba(80,70,40,0.5)",
                 data: [45, 67, 34, 67, 55, 76, 56]
@@ -46,14 +47,14 @@ let barChart = new Chart(ctx2, {
         labels: months,
         datasets: [
             {
-                label: "First Line Chart",
+                label: "2019",
                 fill: false,
                 lineTension: 0,
                 backgroundColor: "rgba(50,45,100,0.5)",
                 data: [65, 59, 67, 45, 67, 55, 56]
             },
             {
-                label: "Second Line Chart",
+                label: "2020",
                 lineTension: 0,
                 backgroundColor: "rgba(80,70,40,0.5)",
                 data: [45, 67, 34, 67, 55, 76, 56]
@@ -72,3 +73,58 @@ let barChart = new Chart(ctx2, {
     }
 });
 
+let radarChart = new Chart(ctx3, {
+    type: 'radar',
+    data: {
+        labels: months,
+        datasets: [
+            {
+                label: "2019",
+                lineTension: 0,
+                backgroundColor: "rgba(50,45,100,0.5)",
+                data: [65, 59, 67, 45, 67, 55, 56]
+            },
+            {
+                label: "2020",
+                lineTension: 0,
+                backgroundColor: "rgba(80,70,40,0.5)",
+                data: [45, 67, 34, 67, 55, 76, 56]
+            }
+        ]
+    },
+    options: {
+        showLines: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+let donutChart = new Chart(ctx4, {
+    type: 'doughnut',
+    data: {
+        labels: ["red", "orange", "yellow"],
+        datasets: [
+            {
+                label: "Favorite Color",
+                lineTension: 0,
+                backgroundColor: "rgba(50,45,100,0.5)",
+                data: [50, 25, 25]
+            }
+        ]
+    },
+    options: {
+        showLines: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
